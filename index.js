703c42ad685d8858a26ee1c22b11d98fe523801b
Alexander Jenkins <acjenkins6440@gmail.com>
2017-07-31 15:53:54 -0700

    Added Codepen files

---
+++ index.js
@@ -0,0 +1,9 @@
+var X = document.createElement("img");
+img.src = "./Picture1.png";
+function PutX(){
+  $(".ul").html('<img src="Picture1.png">');
+}
+var xOrO = 0;
+$(document).ready(function(){
+  $('.u').click(PutXOrO);
+}
